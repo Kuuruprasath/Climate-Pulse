b33df7ab2f6d5671f0f0d4199ef9c718b3cb4cf3
knel0004 <knel0004@student.monash.edu>
2024-08-20 22:08:35 +0800

    Test

---
+++ Conversions/convertt.py
@@ -0,0 +1,31 @@
+
+def suburb_to_lat_long(suburb_name):
+    import pandas as pd
+
+    suburbs = pd.read_csv("../Actual_datasets/SuburbClustered.csv")
+    suburbs
+    filter = suburbs[suburbs["OfficialNameSuburb"] == suburb_name]
+    latitude = filter["Latitude"]
+    longtitude = filter["Longitude"]
+    return [latitude,longtitude]
+
+def suburb_to_ClusterID(suburb_name):  
+    import pandas as pd
+
+    suburbs = pd.read_csv("../Actual_datasets/SuburbClustered.csv")
+    suburbs
+    filter = suburbs[suburbs["OfficialNameSuburb"] == suburb_name]
+    clusterID = filter["ClusterID"]
+    return clusterID
+
+def clusterID_to_variables(clusterID):
+    import pandas as pd
+  
+    weather = pd.read_csv("../Actual_datasets/WeatherData.csv")
+    filter = weather[weather["ClusterID"] == clusterID]
+    # mintemp = filter["TemperatureMin"]
+    # maxtemp = filter["TemperatureMax"]
+    # avgtemp = filter["TemperatureMean"]
+    # rain = filter["RainSum"]
+    filterdf = pd.DataFrame(filter)
+    return filterdf
