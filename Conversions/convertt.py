d33d4781efa49114f3e32b3243d756f5866f9b0d
knel0004 <knel0004@student.monash.edu>
2024-09-03 09:50:43 +0800

    convertt.py changed to use databases

--- Conversions/convertt.py
+++ Conversions/convertt.py
@@ -50,24 +50,43 @@ def suburb_to_ClusterID(suburblist):
     cursor.close()
     conn.close()
     return dataset
-print(suburb_to_ClusterID(["Alpine","Clayton"]))
+# print(suburb_to_ClusterID(["Alpine","Clayton"]))
 
 def clusterID_to_variables(clusterList):  
-    import pandas as pd
+    # import pandas as pd
 
-    weather = pd.read_csv("../Actual_datasets/WeatherData.csv")
-    filter = weather[weather["clusterid"].isin(clusterList) ]
-    # mintemp = filter["TemperatureMin"]
-    # maxtemp = filter["TemperatureMax"]
-    # avgtemp = filter["TemperatureMean"]
-    # rain = filter["RainSum"]
-    filterdf = pd.DataFrame(filter)
-    return filterdf
+    # weather = pd.read_csv("../Actual_datasets/WeatherData.csv")
+    # filter = weather[weather["clusterid"].isin(clusterList) ]
+    # # mintemp = filter["TemperatureMin"]
+    # # maxtemp = filter["TemperatureMax"]
+    # # avgtemp = filter["TemperatureMean"]
+    # # rain = filter["RainSum"]
+    # filterdf = pd.DataFrame(filter)
+    # return filterdf
+
+    import psycopg2
+    clusterlist = tuple(clusterList)
+    conn = psycopg2.connect(
+        dbname="climatepulse",
+        host="postgres-1.c96iysms626t.ap-southeast-2.rds.amazonaws.com",
+        port=5432,
+        user="postgres",
+        password="Climatepulse123."
+    )
+    cursor = conn.cursor()
+    query = "SELECT temperaturemean,rainsum FROM weatherdata WHERE clusterid IN %s"
+    cursor.execute(query, (clusterlist,))
+    dataset = cursor.fetchall()
+    temperature = [row[0] for row in dataset]
+    rainfall =  [row[1] for row in dataset]
+
+    cursor.close()
+    conn.close()
+    return temperature,rainfall
 # print(clusterID_to_variables([10849,106402]))
 def suburb_to_var(suburblist):
-    import pandas as pd
 
     ClusterID = suburb_to_ClusterID(suburblist)
-    variables = clusterID_to_variables(ClusterID)
-    return variables
-# print(suburb_to_var(["Alpine","Clayton"]))
+    temperature,rainfall = clusterID_to_variables(ClusterID)
+    return temperature, rainfall
+print(suburb_to_var(["Alpine","Clayton"]))
